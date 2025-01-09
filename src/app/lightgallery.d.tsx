68b8b4fe3f07e380793dd1be2b06550281d5575e
hovanco <hovanco.vn@gmail.com>
2025-01-09 14:22:24 +0700

    Create MyLightGallery component

---
+++ src/app/lightgallery.d.tsx
@@ -0,0 +1,14 @@
+declare module "lightgallery/plugins/thumbnail" {
+  const lgThumbnail: any;
+  export default lgThumbnail;
+}
+
+declare module "lightgallery/plugins/zoom" {
+  const lgZoom: any;
+  export default lgZoom;
+}
+
+declare module "lightgallery/plugins/video" {
+  const lgVideo: any;
+  export default lgVideo;
+}
